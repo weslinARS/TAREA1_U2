d59332049e50d553fadd9a4114b5498e2f3aab40
weslinARS <weslin.rodriguez@est.ulsa.edu.ni>
2022-08-20 20:09:16 -0600

    TRABAJO TERMINADO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'TAREA1_U2';
+  num1: number | any;
+  num2: number | any;
+  res: number | any;
+  constructor() {}
+  ngOnInit(): void {}
+  suma(): void {
+    this.res = this.num1 + this.num2;
+  }
+  resta() {
+    this.res = this.num1 - this.num2;
+  }
+  mult() {
+    this.res = this.num1 * this.num2; 
+  }
+  division() {
+    this.res = this.num1 / this.num2; 
+  }
 }
